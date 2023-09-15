053d436bcfce67f85bfc3f08ffe75590c1df3b36
Jose Llancamil <jose.llancamil@usach.cl>
2023-09-15 02:27:27 -0300

    Inicialización del proyecto.
    Se crea el esqueleto del proyecto de acuerdo a lo visto en clases.

---
+++ tingeso1/src/test/java/com/topeducation/tingeso1/Tingeso1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topeducation.tingeso1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tingeso1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
